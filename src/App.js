e53c51bc7accd51dce866f40f27b018958503f4f
Florencio <florentbaleinier@gmail.com>
2023-03-21 22:29:27 +0100

    added new <p> on App function

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, this is my first react project
+          Hello, this is my first react project!
         </p>
         <a
           className="App-link"

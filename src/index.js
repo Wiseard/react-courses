f4c724207c9ca15d65f3d6ee0d67296bc184273a
Florencio <florentbaleinier@gmail.com>
2023-03-23 17:22:04 +0100

    adding css file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Greeting = () => {
-  return <>Greeting!</>
-}
+// CSS IMPORT
+import './index.css'
+
+const Booklist = () => (
+  <section className="booklist">
+    <Book />
+    <Book />
+    <Book />
+  </section>
+)
+const Book = () => (
+  <article className="book">
+    <Image />
+    <Title />
+    <Author />
+  </article>
+)
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81fyoFoaxlL._AC_UL900_SR900,600_.jpg"
+    alt="Dog Man: Twenty Thousand Fleas Under the Sea"
+  />
+)
+const Title = () => <h2>Dog Man: Twenty Thousand Fleas Under the Sea</h2>
+const Author = () => <h4>Dav Pilkey</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<Booklist />)
